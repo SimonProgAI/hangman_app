6ca005633c558d53e51450b4f27aa4ba99058c7f
Simon Lupien <lupiensimon@hotmail.com>
2025-05-31 09:59:52 -0500

    modified:   src/components/WordDisplay.js

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,5 +1,12 @@
 import './components.css';
-
+//If user input === one of the letters
+//array.find 
+//array.findIndex 
+//array.includes 
+//array.indexOf -- find the index of the user input, if found, then reveal that letter
+//array.some
+//map.has
+//map.delete
 const WordDisplay = ({wordArr}) => {
     
     const processedWordArr = wordArr.map((letter)=>{
