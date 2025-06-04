82a4e823473ded32f00aaf2a85af90b55d200df1
Simon Lupien <lupiensimon@hotmail.com>
2025-06-03 21:04:59 -0500

    differentiation between hidden and revealed letters in console.log

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,24 +1,38 @@
 import './components.css';
-//If user input === one of the letters
-//array.find 
-//array.findIndex 
-//array.includes 
-//array.indexOf -- find the index of the user input, if found, then reveal that letter
-//array.some
-//map.has
+//If user input === one of the letters --> letter reveals
+//array.find -- Returns the first element that satisfies the provided testing function. Undefined if not.
+//array.findIndex -- Returns the index of the first element that satisfies testing functions. Returns -1 if not
+//array.indexOf -- Returns the first index at which a given element can be found, or -1
+////array.includes -- If contains a certain value, returns true or false.
+//map.has --
 //map.delete
 const WordDisplay = ({wordArr}) => {
+
+    const userInput = "l";
+    //let test = wordArr.findIndex((letter) => letter===userInput );
     
-    const processedWordArr = wordArr.map((letter)=>{
+    for(const [index, value] of wordArr.entries()){
+        //console.log(`index: ${index}, value: ${value}`);
+        if(value===userInput){
+            console.log(`Letter revealed; index ${index}, value: ${value}, user input: ${userInput}.`)
+            //the letter is seen
+        }else {
+            console.log(`Letter hidden; index: ${index}, value: ${value}.`)
+            //the letter is hidden
+        }
+    }                      
+    
+
+    /*const processedWordArr = wordArr.map((letter)=>{
         return(
             <span className="individual_letters">{letter}</span>
         );
-    });
+    });*/ 
 
     return(
         <div>
             WordDisplay
-            <div>{processedWordArr}</div>
+            <div>{}</div>
         </div>
     );
 }
