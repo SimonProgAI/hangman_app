75c6816ecad7f2e6c60693a0e4306d336d4372b2
Simon Lupien <lupiensimon@hotmail.com>
2025-06-04 10:00:54 -0500

    visibilityArr rewrote into a arr of bools

--- src/components/WordDisplay.js
+++ src/components/WordDisplay.js
@@ -1,32 +1,44 @@
 import './components.css';
+import react from 'react';
+import {useState, useEffect} from 'react';
 //If user input === one of the letters --> letter reveals
 //array.find -- Returns the first element that satisfies the provided testing function. Undefined if not.
 //array.findIndex -- Returns the index of the first element that satisfies testing functions. Returns -1 if not
 //array.indexOf -- Returns the first index at which a given element can be found, or -1
-////array.includes -- If contains a certain value, returns true or false.
+//array.includes -- If contains a certain value, returns true or false.
 //map.has --
 //map.delete
 const WordDisplay = ({wordArr}) => {
 
     const userInput = "l";
-    //let test = wordArr.findIndex((letter) => letter===userInput );
+    const [visibilityArr, setVisibilityArr] = useState([]);
+    const [visibilityStyle, setVisibilityStyle] = useState({})
     
-    /*const processedWordArr = wordArr.map((letter)=>{
-        return(
-            <span className="individual_letters">{letter}</span>
-        );
-    });*/ 
-    
-    for(const [index, value] of wordArr.entries()){
-        //console.log(`index: ${index}, value: ${value}`);
-        if(value===userInput){
-            console.log(`Letter revealed; index ${index}, value: ${value}, user input: ${userInput}.`)
-            //the letter is seen
-        }else {
-            console.log(`Letter hidden; index: ${index}, value: ${value}.`)
-            //the letter is hidden
+    useEffect(()=>{
+        
+        wordArr.forEach((letter, index) => {
+            if(letter === userInput){
+                console.log(`Letter ${letter} at index ${index} is revealed`);
+                visibilityArr.push(true)
+            }else{
+                console.log(`Letter ${letter} at index ${index} is hidden`);
+                visibilityArr.push(false)
+            }
+            setVisibilityArr(visibilityArr);
         }
-    }                      
+        );
+    },[wordArr,visibilityArr])
+    console.log(visibilityArr);
+    //create a function that maps over wordArr 
+    //and conditionally renders each letter based on the value in visibilityArr
+    const letterRenderer = ()=>{
+        wordArr.map(()=>{
+
+        })
+
+    }
+
+
 
     return(
         <div>
