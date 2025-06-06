42e502b7983486f04f9cd2a9d5fe831776f0eb54
Simon Lupien <lupiensimon@hotmail.com>
2025-06-06 16:16:06 -0500

    HangmanStickFigure logic is working

--- src/components/HangmanStickFigure.js
+++ src/components/HangmanStickFigure.js
@@ -1,18 +1,26 @@
 import React, { useRef, useEffect } from 'react';
 
 const HangmanStickfigure = () => {
-    const headRef = useRef(null);
     
+    const count = 5;
+    
+    const headColor = count>0 ? 'black': 'white';
+    const torsoColor = count>=1 ? 'black': 'white';
+    const leftArmColor = count>=2 ? 'black': 'white';
+    const rightArmColor = count>=3 ? 'black': 'white';
+    const leftLegColor = count>=4 ? 'black': 'white';
+    const rightLegColor = count>=5 ? 'black': 'white';
+
 
     return (
         <div>
             <svg width="300" height="300" >
-                <circle cx="140" cy="80" r="20" style={{fill: 'black'}}/>
-                <line x1="140" y1="150" x2="140" y2="98"  style={{stroke: 'black', strokeWidth: '8', strokeLinecap: 'round'}}/>
-                <line x1="140" y1="110" x2="105" y2="130" style={{stroke: 'black', strokeWidth: '6', strokeLinecap: 'round'}}/>
-                <line x1="140" y1="110" x2="175" y2="130" style={{stroke: 'black', strokeWidth: '6', strokeLinecap: 'round'}}/>
-                <line x1="140" y1="150" x2="105" y2="190" style={{stroke: 'black', strokeWidth: '6', strokeLinecap: 'round'}}/>
-                <line x1="140" y1="150" x2="175" y2="190" style={{stroke: 'black', strokeWidth: '6', strokeLinecap: 'round'}}/>
+                <circle cx="140" cy="80" r="20" style={{fill: headColor}}/>
+                <line x1="140" y1="150" x2="140" y2="98"  style={{stroke: torsoColor, strokeWidth: '8', strokeLinecap: 'round'}}/>
+                <line x1="140" y1="110" x2="105" y2="130" style={{stroke: leftArmColor, strokeWidth: '6', strokeLinecap: 'round'}}/>
+                <line x1="140" y1="110" x2="175" y2="130" style={{stroke: rightArmColor, strokeWidth: '6', strokeLinecap: 'round'}}/>
+                <line x1="140" y1="150" x2="105" y2="190" style={{stroke: leftLegColor, strokeWidth: '6', strokeLinecap: 'round'}}/>
+                <line x1="140" y1="150" x2="175" y2="190" style={{stroke: rightLegColor, strokeWidth: '6', strokeLinecap: 'round'}}/>
 
                 <line x1="140" y1="25" x2="140" y2="60 " style={{stroke: 'brown', strokeWidth: '3'}}/>
                 <line x1="120" y1="25" x2="55" y2="80" style={{stroke: 'black', strokeWidth: '6'}}/>
