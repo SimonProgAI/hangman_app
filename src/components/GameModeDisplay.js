914899617df17d58fc27632579f7b32b10d95f08
Simon Lupien <lupiensimon@hotmail.com>
2025-06-19 16:31:20 -0500

    Random Word API working with proper validation

--- src/components/GameModeDisplay.js
+++ src/components/GameModeDisplay.js
@@ -2,7 +2,6 @@ import { useRef, useState, useEffect, useMemo } from "react";
 import LetterInputDisplay from "./LetterInputDisplay";
 import HangmanStickfigure from "./HangmanStickFigure";
 //TO-DO 
-    //Random word API 
     //HangmanStickfigure should not show traces of white limbs over black limbs 
     //Return to initial state after GameOver/GameWon screen
     //Persistence before GameWon/GameOver screen (userWord, wordArr, count, guessedLetters, visibilityArr)
@@ -11,16 +10,16 @@ import HangmanStickfigure from "./HangmanStickFigure";
 function GameModeDisplay(){
 //USER_CREATED_WORD 
     const userWordRef = useRef();
-    const errMsgRef = useRef();
+    const errMsgRef2 = useRef();
     const [userWord, setUserWord] = useState("");
-    const [errMsg, setErrMsg] = useState("");
+    const [errMsg2, setErrMsg2] = useState("");
     const [word, setWord] = useState("");
-    
+    const [isDisabled, setIsDisabled] = useState(false);
     
     const createUserWord = ()=>{ 
         //console.log(`function createUserWord called`);
-        
         const userWord = userWordRef.current.value;
+        
         function hasOnlyLetters(word){
             return /^[a-zA-Z]+$/.test(word)
         }
@@ -28,49 +27,53 @@ function GameModeDisplay(){
             return /^[a-zA-Z-]+$/.test(word)
         }
         if(userWord.length<1){
-            setErrMsg("Word must contain at least one letter.")
+            setErrMsg2("Word must contain at least one letter.")
         }else if(userWord.includes("--")){
-            setErrMsg("Word cannot contain two consecutive hyphens.")
+            setErrMsg2("Word cannot contain two consecutive hyphens.")
         }else if (userWord.startsWith("-")||userWord.endsWith("-")){
-            setErrMsg("Word cannot begin or end with an hyphen.")
+            setErrMsg2("Word cannot begin or end with an hyphen.")
         }else if (hasOnlyLetters(userWord)||hasOnlyLettersAndHyphen(userWord)){
-            setUserWord(userWord.toUpperCase());
+            setUserWord(userWord);
+            setIsDisabled(true)
         }else{
-            setErrMsg("Word must contain letters and can contain one hyphen.")
+            setErrMsg2("Word must contain letters and can contain one hyphen.")
         }
         userWordRef.current.value="";
-        setWord(userWord);
+        setWord(userWord.toUpperCase());
         return userWord;
     }
     //console.log(`userWord is set to ${userWord}`);
     
-
 //RANDOM_WORD_API
-    
     const [randomWord, setRandomWord] = useState("");
     const randomWordLengthRef = useRef();
+    const [errMsg1, setErrMsg1] = useState("");
+    const errMsgRef1 = useRef();
 
     const handleRandomWord = () => {
-        //console.log('function handleRandomWord called')
-        
         const randomWordLength = Number(randomWordLengthRef.current.value);
-        const url = `https://random-word-api.herokuapp.com/word?length=${randomWordLength}`;
-        const errorAlert = 'Something went wrong while fetching your request. Please try again.';
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const randomWord = data[0];
-                setRandomWord(randomWord);
-                setWord(randomWord.toUpperCase());
-            })
-            .catch((error)=>alert(errorAlert));
-          
+        if(randomWordLength<2||randomWordLength>16){
+            setErrMsg1("The word must contain between 2 and 16 characters.")
+        }else if (randomWordLength>1&&randomWordLength<17){
+            setIsDisabled(true);
+            const url = `https://random-word-api.herokuapp.com/word?length=${randomWordLength}`;
+            const errorAlert = 'Something went wrong while fetching your request. Please try again.';
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    const randomWord = data[0];
+                    setRandomWord(randomWord);
+                    setWord(randomWord.toUpperCase());
+                })
+                .catch((error)=>alert(errorAlert));
+        }
+        //console.log('function handleRandomWord called')
     }
     //console.log(`randomWord is set to ${randomWord}`);
 
 //WORDARR
     let wordArr = useMemo(()=>word.split(''),[word]);
-    console.log(`word set to ${word}`)
+    //console.log(`word set to ${word}`)
     //console.log(wordArr);
 //USER_INPUT    
     const [userInput, setUserInput] = useState("");
@@ -80,11 +83,12 @@ function GameModeDisplay(){
     const handleUserInput = (letter, index) => {
         
         setUserInput(letter);
+        const upperCaseLetters = letter.toUpperCase()
        
-        if(wordArr.includes(letter)===false){
+        if(wordArr.includes(upperCaseLetters)===false){
             setCount(count+1);
-        }else if(wordArr.includes(letter)===true){
-             setGuessedLetters([...guessedLetters, letter]) 
+        }else if(wordArr.includes(upperCaseLetters)===true){
+             setGuessedLetters([...guessedLetters, upperCaseLetters]) 
         } 
         //console.log(`guessedLetters: ${guessedLetters}`)
         //console.log(`count is ${count+1}`); //count+1 is a workaround solution, maybe rework later
@@ -147,14 +151,14 @@ function GameModeDisplay(){
         <div>
             <div>
                 <span>
-                <button onClick={handleRandomWord}>Random Word</button>
-                <input ref={randomWordLengthRef} placeholder="letters" type="number" min="2" max="10"/>
+                <button onClick={handleRandomWord} disabled={isDisabled}>Random Word</button>
+                <input ref={randomWordLengthRef} placeholder="letters" type="number" min="2" max="16" disabled={isDisabled}/><span ref={errMsgRef1}>{errMsg1}</span>
                 </span>
             </div> 
             <div>
                 <span>
-                    <button onClick={createUserWord}>User Selected</button>
-                    <input type="text" ref={userWordRef}></input><span ref={errMsgRef}>{errMsg}</span>
+                    <button onClick={createUserWord} disabled={isDisabled}>User Selected</button>
+                    <input type="text" ref={userWordRef} disabled={isDisabled}></input><span ref={errMsgRef2}>{errMsg2}</span>
                 </span>
             </div>
             <div>
